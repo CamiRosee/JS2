bf9466492029155e0c3933c3d5c9ec13745958d6
Camila <camila.roserrano@gmail.com>
2023-10-22 16:33:33 +0900

    Uploading Arrow Functions exercise

---
+++ ArrowFunctions/exercise.js
@@ -0,0 +1,30 @@
+//const sum = function (a, b) {
+//  return a + b;
+//}
+
+let sum = (a, b) => a + b
+
+//const subtract = function (a, b) {
+//  return a - b;
+//}
+
+let substract = (a, b) => a - b
+
+//const multiply = function (a, b) {
+//  return a * b;
+//}
+
+let multiply = (a, b) => a * b
+
+//const divide = function (a, b) {
+//  return a / b;
+//}
+
+let divide = (a, b) => a / b
+
+console.log(divide(substract(multiply(sum(2, 4), sum(5, 2)), 2), 5))
+
+
+//const log = function (value) {   
+//  console.log(value);
+//}
