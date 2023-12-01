4fe4eea5d8c6d4ab19e7d32a1acaf8796bab8826
Cami <camila.roserrano@gmail.com>
2023-12-01 17:09:04 +0900

    Uploading Spread operator exercise 1

---
+++ RestSpread1/exercise.js
@@ -0,0 +1,11 @@
+let numberStore = [0, 1, 2];
+let newNumber = 3;
+
+let mergedNumbers = [...numberStore, newNumber]
+
+//numberStore.push(newNumber);
+console.log(numberStore);
+
+console.log(newNumber);
+
+console.log(mergedNumbers);
